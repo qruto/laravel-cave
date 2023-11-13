b55926cabca2e1e43c42716df1467f93cc2ffefe
Slava Razum <razum@qruto.to>
2023-11-13 17:45:25 +0100

    Create EloquentUserProvider with WebAuthn support

---
+++ src/EloquentUserProvider.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Auth;
+
+use App\Auth\Authenticator\Assertion;
+use App\Auth\Controllers\AuthController;
+use App\Auth\Models\AuthKey;
+use Illuminate\Auth\EloquentUserProvider as BaseEloquentUserProvider;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Hashing\Hasher;
+use ParagonIE\ConstantTime\Base64UrlSafe;
+use Throwable;
+use Webauthn\Util\Base64;
+
+class EloquentUserProvider extends BaseEloquentUserProvider
+{
+    public function __construct(private Assertion $assertion, Hasher $hasher, $model)
+    {
+        parent::__construct($hasher, $model);
+    }
+
+    /**
+     * Retrieve a user by the given credentials.
+     */
+    public function retrieveByCredentials(array $credentials): ?Authenticatable
+    {
+        if (! $this->isValidCredentials($credentials)) {
+            return null;
+        }
+
+        $webauthnKey = AuthKey::where('credential_id', Base64UrlSafe::encode(Base64::decode($credentials['id'])))
+            ->orWhere('credential_id', Base64UrlSafe::encodeUnpadded(Base64::decode($credentials['id'])))
+            ->first();
+
+        if (! $webauthnKey) {
+            return null;
+        }
+
+        return $this->retrieveById($webauthnKey->user_id);
+    }
+
+    /**
+     * Check if the credentials are for a public key signed challenge.
+     */
+    protected function isValidCredentials(array $credentials): bool
+    {
+        return isset($credentials['id'], $credentials['rawId'], $credentials['type'], $credentials['response']);
+    }
+
+    /**
+     * Validate a user against the given credentials.
+     */
+    public function validateCredentials(Authenticatable $user, array $credentials): bool
+    {
+        if ($this->isValidCredentials($credentials) && session()->has(AuthController::CREDENTIAL_REQUEST_OPTIONS_SESSION_KEY)) {
+            try {
+                $this->assertion->verify($credentials, session(AuthController::CREDENTIAL_REQUEST_OPTIONS_SESSION_KEY));
+            } catch (Throwable $th) {
+                return false;
+            }
+
+            return true;
+        }
+
+        return false;
+    }
+}
