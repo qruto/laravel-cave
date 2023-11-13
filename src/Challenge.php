966942748bdd69f6da6e4006a443dd58f6de8b42
Slava Razum <razum@qruto.to>
2023-11-13 17:35:51 +0100

    Challenge object

---
+++ src/Challenge.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Auth;
+
+use Stringable;
+
+class Challenge implements Stringable
+{
+    public function __construct(private int $length = 16)
+    {
+    }
+
+    public function __toString()
+    {
+        return random_bytes($this->length);
+    }
+}
