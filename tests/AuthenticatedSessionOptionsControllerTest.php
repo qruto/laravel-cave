4e250d5772bf9a954aa20380b50be9cac4f19376
Slava Razum <razum@qruto.to>
2023-11-14 16:47:00 +0100

    First testcase

--- tests/AuthenticatedSessionOptionsControllerTest.php
+++ tests/AuthenticatedSessionOptionsControllerTest.php
@@ -1,22 +1,23 @@
 <?php
 
 use App\Models\User;
+use Illuminate\Foundation\Auth\User as AuthUser;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Qruto\Cave\Contracts\CreatesNewUsers;
 
 uses(RefreshDatabase::class);
 
 it('generates options for the new user', function () {
+    config()->set('auth.providers.users.model', User::class);
 
-    $response = $this->post('auth/options', ['email' => 'rick@unity.io']);
+    $this->instance(CreatesNewUsers::class, new class implements CreatesNewUsers {
+        public function create(array $input, AuthUser $user): AuthUser
+        {
+            return $user->fill(['name' => $input['name']]);
+        }
+    });
+
+    $response = $this->post('auth/options', ['email' => 'rick@unity.io', 'name' => 'Rick Sanchez']);
 
     expect(User::where('email', 'rick@unity.io')->exists())->toBeTrue();
-
-//    $this->assertDatabaseHas('users', [
-//        'id' => $user->id,
-//        'current_team_id' => null,
-//        'email' => $user->email,
-//        'email_verified_at' => null,
-//        'verified_at' => null,
-//        'remember_token' => null,
-//    ]);
 });
