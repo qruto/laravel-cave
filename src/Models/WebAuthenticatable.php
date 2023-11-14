9e055585083222aa8c559c7dc1cbe131bee75849
Slava Razum <razum@qruto.to>
2023-11-14 16:47:58 +0100

    WebAuthenticatable trait

---
+++ src/Models/WebAuthenticatable.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Qruto\Cave\Models;
+
+use BadMethodCallException;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Webauthn\PublicKeyCredentialUserEntity;
+
+/** @mixin Model */
+trait WebAuthenticatable
+{
+    public function getPassword()
+    {
+        throw new BadMethodCallException('Deprecated. Use `authKeys` relation to get stored credentials.');
+    }
+
+    public function newPublicKeyCredentialUserEntity(string $email)
+    {
+        $entity = self::create([
+            'email' => $email,
+        ]);
+
+        return PublicKeyCredentialUserEntity::create(
+            $entity->email,
+            $entity->id,
+            $entity->email,
+            // TODO: create icon
+            null,
+        );
+    }
+
+    /**
+     * Get the webauthn keys associated to this user.
+     */
+    public function passkeys(): HasMany
+    {
+        return $this->hasMany(Passkey::class);
+    }
+}
